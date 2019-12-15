2e7626cd26b4360af91add253084953466fba3b3
DejaLFlynn <dejaflynn@pursuit.org>
2019-12-15 11:25:18 -0500

    21 buttons working

--- blackJack.js
+++ blackJack.js
@@ -1,41 +1,41 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-  
-    const buttons = async()=>{
-        try{
-        let draw = document.querySelector("#drawCard")
-        draw.addEventListener("click",()=>{
-            drawCard()
-        })
-        let hit = document.querySelector("#hitCard")
-        hit.addEventListener("click",()=>{
-            hitCard()
-        })
-    } catch(err){
-        console.log("error")
+document.addEventListener('DOMContentLoaded', () => {
+    const newDeckCards = async () => {
+        try {
+            let newDeck = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+       
+            let id = document.querySelector("#deckId")
+            id.innerText = newDeck.data.deck_id
+// debugger 
+            let button = document.querySelector('#button')
+            button.addEventListener("click", (displayCard));
+            let button2 = document.querySelector("#button2") 
+            button2.addEventListener("click", (display2Cards))    
+            // debugger
+        } catch (err) {
+            console.log(err)
+            debugger
+        }
     }
-}
-const hitCard = async()=>{
-let id = document.querySelector("#drawId")
-let hit = await axios.get(`https://deckofcardsapi.com/api/deck/${id.innerText}/draw/?count=1`)
-hit.data.cards.forEach(el=>{
-    let image =document.createElement("img")
-    image.src =el.image
-    document.body.appendChild(image)
-})
-}
-const drawCard = async() => {
-    let id = document.querySelector("#handOfCards")
-    let drawCard = await axios.get(`https://deckofcardsapi.com/api/deck/${id.innerText}/draw/?count=2`);
-
-    drawCard.data.cards.forEach(el=>{
-        let image = document.createElement("img");
-        image.src = el.image;
-        document.body.appendChild(image)
-    })
-    }
-
-    hitCard()
-    drawCard()
-    buttons()
-   
+            const displayCard = async() => {
+            let id = document.querySelector("#deckId")
+            let drawCard = await axios.get(`https://deckofcardsapi.com/api/deck/${id.innerText}/draw/?count=1`);
+            drawCard.data.cards.forEach(el=>{
+                let image = document.createElement("img");
+                image.src = el.image;
+                document.body.appendChild(image)
+            })
+            }
+            const  display2Cards = async()=>{
+                let id = document.querySelector("#deckId")
+                let drawCard = await axios.get(`https://deckofcardsapi.com/api/deck/${id.innerText}/draw/?count=2`);
+                drawCard.data.cards.forEach(el=>{
+                    let image = document.createElement("img");
+                    image.src = el.image;
+                    document.body.appendChild(image)
+                })   
+            }
+            const startGame = async()=>{
+            
+            }
+    newDeckCards()
 })
