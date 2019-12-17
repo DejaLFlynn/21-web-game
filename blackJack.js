152cc29e07e03d2056b47ad2fb953e2005a469a2
DejaLFlynn <dejaflynn@pursuit.org>
2019-12-16 21:26:37 -0500

    working on putting back values

--- blackJack.js
+++ blackJack.js
@@ -1,43 +1,49 @@
 document.addEventListener('DOMContentLoaded', () => {
+    let total = 0
+    let newArr =[]
     const newDeckCards = async () => {
         try {
             let newDeck = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
             let id = document.querySelector("#deckId")
             id.innerText = newDeck.data.deck_id
-           
             let button = document.querySelector('#button')
             button.addEventListener("click", (displayCard));
             let button2 = document.querySelector("#button2")
             button2.addEventListener("click", () => {
-                startGame()
-        
-                button2.parentNode.removeChild(button2)
+            startGame()
+            button2.parentNode.removeChild(button2)
             })
             let stay = document.querySelector("#stay")
             stay.addEventListener("click", () => {
-              
-                stay.parentNode.removeChild(stay)
+            stay.parentNode.removeChild(stay)
             })
         } catch (err) {
-            console.log(err)
-            
+            console.log(err) 
         }
     }
-    const handTotal = (total, results) => {
-        let playerScore = 0;
-        let dealerScore = 0;
-        let result = document.querySelector("#displayCard")
-        let total = document.createElement("h3")
-        if (playerScore > dealerScore && playerScore <= 21) {
-            winningHand()
-        } else if (dealerScore < 21 && dealerScore > playerScore) {
-            dealerWin()
-        } else if (playerScore > 21 && dealerScore > 21) {
-            tie()
+    
+    const handTotal = (arr) => {
+      
+       arr.forEach(el=>{
+        if (el.value === "KING" || el.value === "QUEEN" || el.value === "JACK") {
+            el.value = 10
+            total +=el.value;
+        } else if (el.value == "ACE") {
+            if(total <11){
+                el.value = 11;
+                total +=el.value
+            } else if(total === 20){
+                el.value = 1;
+                total += el.value
+            }
+        }else{
+            total +=Number(el.value)
         }
-        
-    }
+       })
+       res += total;
+       newArr = []
 
+    }
     const displayCard = async () => {
         let id = document.querySelector("#deckId")
         let drawCard = await axios.get(`https://deckofcardsapi.com/api/deck/${id.innerText}/draw/?count=1`);
@@ -46,35 +52,17 @@ document.addEventListener('DOMContentLoaded', () => {
             image.src = el.image;
             drawCard.innerText = ""
             document.body.appendChild(image)
-            if (el.value === "KING" || el.value === "QUEEN" || el.value === "JACK") {
-                if (results.id === "displayCards") {
-                    playerScore += 10;
-                } else {
-                    dealerScore += 10
-                }
-            } else if (el.value == "ACE") {
-                if (results.id === "displayCards") {
-                    playerScore += 11
-                } else {
-                    dealerScore += 11
-                }
-            } else {
-                if (results.id === "displayCards") {
-                    playerScore += Number(el.value)
-                } else {
-                    dealerScore += Number(el.value)
-                }
-            }
             let li = document.createElement("li")
             li.appendChild(img)
-            results.appendChild(li)
+            let result = document.querySelector("#results")
             drawCard.parentNode.appendChild(results)
-        })
+            let total = document.querySelector("p")
+            total.id = "total"
+            result.appendChild(total)
+        })   
 
     }
     const startGame = async () => {
-        let playerScore = 0;
-        let dealerScore = 0;
         let id = document.querySelector("#deckId")
         let drawCard = await axios.get(`https://deckofcardsapi.com/api/deck/${id.innerText}/draw/?count=2`);
         drawCard.data.cards.forEach(el => {
@@ -84,33 +72,39 @@ document.addEventListener('DOMContentLoaded', () => {
 
         })
         computerGame()
+        handTotal()
+        winningHand()
     }
     const computerGame = async () => {
-        let playerScore = 0;
-        let dealerScore = 0;
         let id = document.querySelector("#deckId")
-        let drawCard = await axios.get(`https://deckofcardsapi.com/api/deck/${id.innerText}/draw/?count=2`);
+        let drawCard = await axios.get(`https://deckofcardsapi.com/api/deck/${id.innerText}/draw/?count=3`);
         drawCard.data.cards.forEach(el => {
             let image = document.createElement("img");
             image.src = el.image;
             document.body.appendChild(image)
 
         })
+        handTotal()
+        winningHand()
+
     }
 
     const winningHand = () => {
-        let results = document.querySelector("#results")
-        let playerWins = document.createElement("h3")
-        results.innerText = "Winner"
-        results.appendChild(playerWins)
-    }
-    const tie = () => {
-        let sameScore = document.createElement("h4")
-        sameScore.innerText = "Tie"
-        resultsDiv.replaceChild(sameScore, handOfCards)
+        let dealerScore = document.querySelector("#computerCard")
+        let playerScore = document.querySelector("#displayCard")
+        let result = document.querySelector("playerResults")
+        if (playerScore > dealerScore && playerScore <= 21) {
+            results.innerText = "Over"
+        } else if (dealerScore < 21 && dealerScore > playerScore) {
+            result.innerText = "player wins"
+        } else if (playerScore > 21 && dealerScore > 21) {
+            result.innerText = "tie"
+        }
+        
     }
     newDeckCards()
     handTotal()
+    winningHand()
  
 
 })
