2f4a11dafec2327e60e0e04baa01fbe2b8fd41b2
DejaLFlynn <dejaflynn@pursuit.org>
2019-12-27 13:05:06 -0500

    more refactoring, working on rules, have values

---
+++ 21final.js
@@ -0,0 +1,99 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let stay = document.querySelector("#stay")
+    let startGame = document.querySelector("#startGame")
+
+
+    stay.addEventListener("click", () => {
+
+    })
+    startGame.addEventListener("click", () => {
+        gameDeck()
+        computerDeck()
+        startGame.parentNode.removeChild(startGame)
+    })
+
+    const gameDeck = async () => {
+        try {
+            let response = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+            let newDeck = response.data.deck_id
+            console.log(newDeck)
+            let drawResponse = await axios.get(`https://deckofcardsapi.com/api/deck/${newDeck}/draw/?count=2`)
+            let twoCards = drawResponse.data.cards
+            let total = 0
+            twoCards.forEach(el => {
+                // debugger
+                let value = el.value
+                if (value === "KING" || value === "QUEEN" || value === "JACK") {
+                    total = 10
+                } else if (value === "ACE") {
+                    if (total <= 10) {
+                        total += 11
+                    } else {
+                        total += 1
+                    }
+                } else {
+                    total += Number(value)
+                }
+
+            })
+            if (total === 21) {
+                let results = document.querySelector("#results")
+                results = "winner"
+                let h3 = document.createElement("h3")
+                h3.innerText = results
+                startGame.appendChild(h3)
+            } else if (total > 21) {
+                results = "loser"
+                let h3 = document.createElement("h3")
+                h3.innerText = results
+                startGame.appendChild(h3)
+            }
+            let scoreTotal = document.createElement("p")
+            scoreTotal.innerText = total
+
+        } catch (error) {
+            console.log("error")
+        }
+    }
+    const computerDeck = async () => {
+        gameDeck()
+        endGame()
+    }
+    const endGame = () => {
+        let computerScore = document.querySelector("#computerScore")
+        let playerScore = document.querySelector("#playerScore")
+        let scoreResult = document.querySelector("#results")
+        if (playerScore.innerText > computerScore.innerText && playerScore.innerText <= 21) {
+            scoreResult.innerText = "Over"
+        } else if (computerScore.innerText < 21 && computerScore.innerText > playerScore.innerText) {
+            scoreResult.innerText = "player wins"
+        } else if (playerScore.innerText > 21 && computerScore.innerText > 21) {
+            scoreResult.innerText = "tie"
+        }
+    }
+    const hitCard = async () => {
+        hitCard.addEventListener("click", async () => {
+            try {
+                let hitResponse = await axios.get(`https://deckofcardsapi.com/api/deck/${newDeck}/draw/?count=1`)
+                let oneCard = hitResponse.data.cards
+                oneCard.forEach(el => {
+                    let value = el.value
+                    if (value === "KING" || value === "QUEEN" || value === "JACK") {
+                        total = 10
+                    } else if (value === "ACE") {
+                        if (total <= 10) {
+                            total += 11
+                        } else {
+                            total += 1
+                        }
+                    } else {
+                        total += Number(value)
+                    }
+                })
+            } catch (error) {
+                console.log("error")
+            }
+        })
+
+    }
+})
--- newblackjack.js
+++ newblackjack.js
@@ -7,29 +7,33 @@ const newDeckCards = async () => {
         let response = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
         let newDeck = response.data.deck_id
         let drawResponse = await axios.get(`https://deckofcardsapi.com/api/deck/${newDeck}/draw/?count=2`)
-        let playerScore = document.querySelector("playerScore")
-        let total = 0
+        let pScore = document.querySelector("#playerScore")
+        let cScore = document.querySelector("#computerScore")
         drawResponse.data.cards.forEach(el => {
-            debugger
-            let cardTotal = el.value
-            if (cardTotal === "KING" || cardTotal === "QUEEN" || cardTotal === "JACK") {
-                total += 10
-            } else if (cardTotal == "ACE") {
-                if (total < 11) {
-                    total += 11
+            let cardImage = document.createElement("img")
+            cardImage.src = el.image
+            pScore.appendChild(cardImage)
+       
+         total =el.value
+         debugger
+            if (el.value === "KING" || el.value === "QUEEN" || el.value === "JACK") {
+                el.value += 10
+            } else if (el.value === "ACE") {
+                if (el.value < 11) {
+                    el.value += 11
                 } else if (total === 20) {
-                    total += 1
+                    el.value += 1
                 }
             } else {
-                total += Number(cardTotal)
+                el.value += Number(total)
 
             }
-            playerScore.innerText = total
+            // playerScore.innerText = el.value
             
         })
         
         // displayCard.appendChild(playerScore)
-        gameScore(total)
+        // gameScore(total)
  
 
     } catch (err) {
